4bbe191d339ac3306393c73b0c111ebc55e5f83c
Charbel Kisso <charbelkisso@gmail.com>
2026-01-14 14:46:10 +0100

    add smoke test

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { ProfilesModule } from './profiles/profiles.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5435,
-      username: 'user',
-      password: 'password',
-      database: 'devmatch',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10) || 5435,
+      username: process.env.DB_USERNAME || 'user',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_DATABASE || 'devmatch',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
